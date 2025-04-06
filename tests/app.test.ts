c469ffce7825eb2894ff455901868aa117d258a5
abdul-basith-yipli <abdul.basith@kreedalabs.com>
2025-04-06 17:39:41 +0530

    test health

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -6,6 +6,6 @@ describe("GET /", () => {
   it("should return 'Hello, World!'", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Hello, Worldd!");
+    expect(response.text).toBe("Hello, World!");
   });
 });
