7a731d13046053838707c32d030e36bebd42ec54
abdul-basith-yipli <abdul.basith@kreedalabs.com>
2025-04-07 11:06:03 +0530

    app test update for feature flag

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -3,9 +3,16 @@ import app from "../src/app";
 // Import the app from the main application file
 
 describe("GET /", () => {
-  it("should return 'Hello, World!'", async () => {
-    const response = await request(app).get("/");
-    expect(response.status).toBe(200);
-    expect(response.text).toBe("Hello, World!");
-  });
+  if (process.env.FEAT_HEALTH_CHECK === "true") {
+    it("should return 'Hello, World!' when health check is enabled", async () => {
+      const response = await request(app).get("/");
+      expect(response.status).toBe(200);
+      expect(response.text).toBe("Hello, World!");
+    });
+  } else {
+    it("should return 404 when health check is disabled", async () => {
+      const response = await request(app).get("/");
+      expect(response.status).toBe(404);
+    });
+  }
 });
